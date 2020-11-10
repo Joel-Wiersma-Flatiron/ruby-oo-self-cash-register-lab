1b7301511f047e684a0bd5bb6cd9a1de6af39f30
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-09 22:24:58 -0700

    commit tree 3797ba2ff93d60cf20f521947c336a1e638c7521

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
-
+class CashRegister
+    attr_accessor :discount, :total, :items
+    def initialize(discount = 0)
+        @total = 0.0
+        @discount = discount
+        @items = []
+        @previous_prices = []
+    end
+    def add_item(title, price, quantity = 1)
+        @total += (price * quantity)
+        i = 0
+        @previous_prices << (price * quantity)
+        while i < quantity
+            @items << title
+            i += 1
+        end
+    end
+    def apply_discount
+        if @discount != 0
+            @total *= (1 - (@discount.to_f / 100))
+            puts "After the discount the total come to $#{@total}."
+            return "After the discount, the total comes to $#{@total.to_i}."
+        else
+            return "There is no discount to apply."
+        end
+    end
+    def void_last_transaction
+        @items.pop
+        @total -= @previous_prices.pop
+    end
+end
